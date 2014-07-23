e6d59cfba737e9429899e44290f3f05708cf9051
Tim 'S.D.Eagle' Zeitz <tim.zeitz@googlemail.com>
2014-07-23 11:11:30 +0200

    small refactoring - cleaned up CLI and additional stats command

--- thefunnel.rb
+++ thefunnel.rb
@@ -10,35 +10,24 @@ store = PStore.new ARGV[0]
 cardset = store.transaction(true) { store[:cardset] }
 cardset ||= FlashcardSet.new
 
+def select_topic topics
+	puts "Choose a topic (by index)"
+	topics = topics.sort
+	puts "0/*: - ALL"
+	topics.each_with_index do |topic, i|
+		puts "#{i + 1}: #{topic.join ' - '}"
+	end
+	selection = $stdin.gets.chomp
+	return [] if selection == '' || selection == '0' || selection == '*'
+	topics[selection.to_i - 1]
+end
+
 if ARGV[1] == 'add'
 	puts "before:", cardset.to_s
 	Parser.new(File.open(ARGV[2]), cardset).parse
 	puts "after:", cardset.to_s
 elsif ARGV[1] == 'ask'
-	old = cardset.to_s
-	puts "Welcome!", "Your Flashcard Box:", old
-	question_count = cardset.total_questions
-
-	(ARGV[2] || 10).to_i.times do 
-		cardset.ask do |question, answers, number_of_answers|
-			puts "\n\n\n\n\n\n"
-			puts "Question: #{question} (#{number_of_answers} answers)".light_cyan, '?????????????????????????????????????????????????????????????????????????????????????'.light_white
-			$stdin.gets.chomp
-			puts "Correct answer was:", answers.green, "Was your answer correct?"
-			next $stdin.gets.chomp == 'y'
-		end
-	end
-
-	puts "Your Flashcard Box:", "Before:", old, "Now:", cardset.to_s, "Goodbye"
-	raise 'Broken card moves' unless question_count == cardset.total_questions
-elsif ARGV[1] == 'ask_topic'
-	puts "Choose a topic (by index)"
-	topics = cardset.topics.sort
-	topics.each_with_index do |topic, i|
-		puts "#{i}: #{topic.join ' - '}"
-	end
-	topic = topics[$stdin.gets.chomp.to_i]
-
+	topic = select_topic cardset.topics
 	old = cardset.topic_stats topic
 	puts "Welcome!", "Your Flashcard Box:", old
 	question_count = cardset.total_questions
@@ -55,19 +44,16 @@ elsif ARGV[1] == 'ask_topic'
 
 	puts "Your Flashcard Box:", "Before:", old, "Now:", cardset.topic_stats(topic), "Goodbye"
 	raise 'Broken card moves' unless question_count == cardset.total_questions
-elsif ARGV[1] == 'update_topic'
+elsif ARGV[1] == 'update'
 	puts "before:", cardset.to_s
-
-	puts "Choose a topic (by index)"
-	topics = cardset.topics.sort
-	topics.each_with_index do |topic, i|
-		puts "#{i}: #{topic.join ' - '}"
-	end
-	topic = topics[$stdin.gets.chomp.to_i]
+	topic = select_topic cardset.topics
 
 	cardset.remove_topic topic
 	Parser.new(File.open(ARGV[2]), cardset, topic).parse
 	puts "after:", cardset.to_s
+elsif ARGV[1] == 'stats'
+	topic = select_topic cardset.topics	
+	puts cardset.topic_stats topic
 end
 
 store.transaction do
